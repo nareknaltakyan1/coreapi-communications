20949894d3aa2bd53bfa067d94d05156a3c94b49
Narek Naltakyan <nareknaltakyan1@gmail.com>
2024-05-21 18:13:15 +0400

    add spotless and spotbugs, also change main class name and package name, add eclipse-format.xml file for formatting standards

---
+++ src/main/java/com/nnaltakyan/core/communications/CommunicationsApplication.java
@@ -0,0 +1,23 @@
+package com.nnaltakyan.core.communications;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class CommunicationsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CommunicationsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void afterStart()
+	{
+		log.debug("Application started");
+	}
+}
--- src/test/java/com/nnaltakyan/core/communications/CommunicationsApplicationTests.java
+++ src/test/java/com/nnaltakyan/core/communications/CommunicationsApplicationTests.java
@@ -1,13 +1,15 @@
-package com.nnaltakyan.api.coreapicommunications;
+package com.nnaltakyan.core.communications;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CoreapiCommunicationsApplicationTests {
+class CommunicationsApplicationTests
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
 	}
 
 }
