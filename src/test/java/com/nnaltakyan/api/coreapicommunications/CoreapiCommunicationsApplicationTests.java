4c61b61bc0567487e26216038e0d5581e91966a4
Narek Naltakyan <nareknaltakyan1@gmail.com>
2024-05-21 18:03:05 +0400

    Initial commit

---
+++ src/test/java/com/nnaltakyan/api/coreapicommunications/CoreapiCommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnaltakyan.api.coreapicommunications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreapiCommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
